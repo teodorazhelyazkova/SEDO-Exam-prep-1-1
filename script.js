f02a9dd044ef1301f63b5cbcf80581454f0d74c1
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2025-10-22 20:58:46 +0300

    3 commits were squashed

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ function add(a, b) {
   return a + b;
 }
 
+// Change 3
 function subtract(a, b) {
   return a - b;
 }
 
+// Change 1
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = add(a, b);
 });
 
+// Change 2
 document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
