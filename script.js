43382d051ab46c086f118044a9fdb33b3ddaae57
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2025-10-23 13:49:02 +0300

    change 5

--- script.js
+++ script.js
@@ -21,3 +21,5 @@ document.getElementById('btn-subtract').addEventListener('click', () => {
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
 });
+
+// Change 5
